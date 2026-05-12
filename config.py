e273df695720faa950028001911cd4bd015b43e6
san <lds2012@gmail.com>
2026-05-12 16:28:31 +0800

    change hf to modelscope

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def get_config():
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
-        "datasource": 'opus_books',
+        "datasource": 'Helsinki-NLP/opus_books',
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
